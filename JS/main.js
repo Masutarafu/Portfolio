10cc92d062b39b60936ffa3f51ef480c73de7e24
FAROOK <112445407+Masutarafu@users.noreply.github.com>
2023-08-14 05:42:46 +0100

    Update main.js
    Present JS code for the somewhat complete landing page.

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,5 @@
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-// menuBtn.addEventListener('click', () => {
-//     if(!menuOpen) {
-//         menuBtn.classList.add('open');
-//         menuOpen = true;
-//     } else {
-//         menuBtn.classList.remove('open');
-//         menuOpen = false;
-//     }
-// });
 
-
-
-
+//Hamburger Menu
 const menuBtn = document.querySelector('.menu-btn');
 const menuContent = document.querySelector('.menu-content');
 let menuOpen = false;
@@ -23,7 +10,6 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('open');
 
     // Set the display property of the menu content element to "block" to show it
-    // You can use any other value like "flex", "grid", or "inline-block" based on your layout requirements
     menuContent.style.display = 'block';
 
     menuOpen = true;
@@ -36,4 +22,46 @@ menuBtn.addEventListener('click', () => {
 
     menuOpen = false;
   }
+});
+
+
+// Fun facts section (Transform and Box-shadow)
+const sections = document.querySelectorAll('.facts-intro-section');
+
+sections.forEach(section => {
+    section.addEventListener('click', () => {
+        sections.forEach(otherSection => {
+            if (otherSection !== section) {
+                otherSection.classList.remove('clicked'); // Remove "clicked" class from other sections
+            }
+        });
+        section.classList.toggle('clicked'); // Toggle "clicked" class on the clicked section
+    });
+});
+
+document.addEventListener('click', (event) => {
+    sections.forEach(section => {
+        if (!section.contains(event.target)) {
+            section.classList.remove('clicked'); // Remove "clicked" class from all sections
+        }
+    });
+});
+
+
+
+// Back To Top Button
+const backToTopButton = document.getElementById('backToTopBtn');
+
+// Show/hide the button based on scroll position
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 270) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+// Scroll to the top when the button is clicked
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
