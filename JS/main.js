e08914f05da5a001a5897c2e76ab0ea153e123a9
FAROOK <112445407+Masutarafu@users.noreply.github.com>
2023-08-19 05:32:10 +0100

    Update main.js
    Biggest JS update so far. It consists of some new functions here and there.

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,141 @@
+// Progressive Loading// Progressive Loading// Progressive Loading// Progressive Loading// Progressive Loading
+// Progressive Loading// Progressive Loading// Progressive Loading// Progressive Loading// Progressive Loading
+// Wait for the page to fully load
+window.addEventListener('load', function() {
+    // Display the priority section with fade-in effect
+    const prioritySection = document.getElementById('preSection');
+    prioritySection.style.display = 'block';
+    setTimeout(function() {
+      prioritySection.style.opacity = 1;
+    }, 500);
+  
+    // Wait for the initial section to appear and then start fading out
+    setTimeout(function() {
+      prioritySection.style.opacity = 0;
+    }, 2000); // Adjust timing as needed
+  
+    // Load the remaining section after transitions are complete
+    setTimeout(function() {
+      prioritySection.style.display = 'none';
+      document.getElementById('mainSection').style.display = 'block';
+    }, 3000); // Adjust timing as needed
+  });  
+    
 
-//Hamburger Menu
+
+//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu
+//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu//Hamburger Menu
 const menuBtn = document.querySelector('.menu-btn');
 const menuContent = document.querySelector('.menu-content');
+const menuLinks = menuContent.querySelectorAll('a'); // Select all links inside the menu
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
-    // Toggle the "open" class to change the appearance of the menu button (optional)
-    menuBtn.classList.add('open');
-
-    // Set the display property of the menu content element to "block" to show it
-    menuContent.style.display = 'block';
-
-    menuOpen = true;
+    openMenu();
   } else {
-    // Toggle the "open" class to change the appearance of the menu button (optional)
-    menuBtn.classList.remove('open');
-
-    // Set the display property of the menu content element to "none" to hide it
-    menuContent.style.display = 'none';
-
-    menuOpen = false;
+    closeMenu();
   }
 });
 
+// Function to open the menu
+function openMenu() {
+  menuBtn.classList.add('open');
+  menuContent.style.display = 'block';
+  menuOpen = true;
+}
 
-// Fun facts section (Transform and Box-shadow)
+// Function to close the menu
+function closeMenu() {
+  menuBtn.classList.remove('open');
+  menuContent.style.display = 'none';
+  menuOpen = false;
+}
+
+// Add event listeners to menu links to close the menu when clicked
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeMenu();
+  });
+});
+
+
+
+//Single Page Working//Single Page Working//Single Page Working//Single Page Working//Single Page Working
+//Single Page Working//Single Page Working//Single Page Working//Single Page Working//Single Page Working
+document.addEventListener("DOMContentLoaded", function () {
+    const section1 = document.getElementById("section1");
+    const section2 = document.getElementById("section2");
+    const section3 = document.getElementById("section3");
+    const section4 = document.getElementById("section4");
+    const aboutMeButton = document.getElementById("aboutMe");
+    const resumeButton = document.getElementById("resume");
+    const porfolioButton = document.getElementById("portfolio");
+    const contactButton = document.getElementById("contact");
+
+    aboutMeButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        section1.style.display = "block";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        section4.style.display = "none";
+    });
+
+    resumeButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        section1.style.display = "none";
+        section2.style.display = "block";
+        section3.style.display = "none";
+        section4.style.display = "none";
+    });
+
+    porfolioButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        section1.style.display = "none";
+        section2.style.display = "none";
+        section3.style.display = "block";
+        section4.style.display = "none";
+    });
+
+    contactButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        section1.style.display = "none";
+        section2.style.display = "none";
+        section3.style.display = "none";
+        section4.style.display = "block";
+    });
+
+});
+
+
+
+// Present Page Link color// Present Page Link color// Present Page Link color// Present Page Link color
+// Present Page Link color// Present Page Link color// Present Page Link color// Present Page Link color
+const links = document.querySelectorAll('.menuLinks');
+
+// Set default color for the first link
+links[0].style.color = '#800080';
+
+// Function to handle the click event
+function changeColor(event) {
+  // Reset the color of all links
+  links.forEach(link => {
+    link.style.color = '#555555';
+  });
+
+  // Change the color of the clicked link
+  event.target.style.color = '#800080';
+}
+
+// Attach the click event listener to each link
+links.forEach(link => {
+  link.addEventListener('click', changeColor);
+});
+
+
+
+// Fun facts section (Transform and Box-shadow)// Fun facts section (Transform and Box-shadow)
+// Fun facts section (Transform and Box-shadow)// Fun facts section (Transform and Box-shadow)
 const sections = document.querySelectorAll('.facts-intro-section');
 
 sections.forEach(section => {
@@ -49,7 +159,8 @@ document.addEventListener('click', (event) => {
 
 
 
-// Back To Top Button
+// Back To Top Button// Back To Top Button// Back To Top Button// Back To Top Button// Back To Top Button
+// Back To Top Button// Back To Top Button// Back To Top Button// Back To Top Button// Back To Top Button
 const backToTopButton = document.getElementById('backToTopBtn');
 
 // Show/hide the button based on scroll position
@@ -65,3 +176,4 @@ window.addEventListener('scroll', () => {
 backToTopButton.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
